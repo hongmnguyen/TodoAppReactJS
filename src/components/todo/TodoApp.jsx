ba7db1fece4d02571a9c8bb9e74d1ca51fc90878
Hong Nguyen <hongsap@gmail.com>
2021-03-25 21:48:50 -0700

    add feature router

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,13 +1,25 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class TodoApp extends Component{
     render(){
         return(
-            <div className="TodoApp">
-                            
-                <LoginComponent/> 
+            <div className="TodoApp"> 
+                <Router>
+                    <Route path ="/" exact component={LoginComponent}/>
+                    <Route path="/login" component={LoginComponent}/>
+                    <Route path="/Welcome" component={WelcomeComponent}/>
+                </Router>     
+                {/* <WelcomeComponent/>
+                <LoginComponent/>  */}
+               
             </div>
         )
+    }
+}
+class WelcomeComponent extends Component{
+    render(){
+        return <div>Welcome in 28mins</div>
     }
 }
 class LoginComponent extends Component{
@@ -24,12 +36,15 @@ class LoginComponent extends Component{
         this.handleChange = this.handleChange.bind(this)
         this.loginClicked= this.loginClicked.bind(this)
     }
-    handleChange(event){
+
+    handleChange(event) {
         //console.log(this.state);
         this.setState(
             {
             [event.target.name]
-            :event.target.value});
+            :event.target.value
+            }
+        )
     }
     // handleUsernameChange(event){
     //     console.log(event.target.value);
@@ -42,12 +57,13 @@ class LoginComponent extends Component{
     //     console.log(event.target.value);
     //     this.setState({password:event.target.value});    
     // }
-    loginClicked(){
+    loginClicked() {
         //in28 mins, dummy
         if(this.state.username=== 'in28mins' && this.state.password==='dummy'){
-            console.log('Successful')
-            this.setState({showSuccessMessage:true})
-            this.setState({hasLoginFailed:false})
+            //console.log('Successful')
+            this.props.history.push("/welcome")
+            //this.setState({showSuccessMessage:true})
+            //this.setState({hasLoginFailed:false})
         }    
         else {
             this.setState({showSuccessMessage:false})
@@ -61,28 +77,29 @@ class LoginComponent extends Component{
     render (){
         return(
             <>
-                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
-                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
-                
+                {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/> */}
+                {this.state.hasLoginFailed &&<div>Invalid Credentials</div>}
+                {/* <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/> */}
+                {this.state.showSuccessMessage &&<div>Login Successful</div>}
                 User Name: <input type = "text123" name ="username" value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type ="password1" name ="password" value={this.state.password} onChange={this.handleChange}/>
-                <button on Click={this.loginClicked}>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </>    
         )
     }
 
 }
-function ShowInvalidCredentials(props) {
-    if(props.hasLoginFailed){
-        return<div>Invalid Credentials</div>
-    }
-    return null   
-}
-function ShowLoginSuccessMessage(props) {
-    if(props.showSuccessMessage){
-        return<div>Login Successful</div>
-    }
-    return null   
-}
+// function ShowInvalidCredentials(props) {
+//     if(props.hasLoginFailed){
+//         return<div>Invalid Credentials</div>
+//     }
+//     return null   
+// }
+// function ShowLoginSuccessMessage(props) {
+//     if(props.showSuccessMessage){
+//         return<div>Login Successful</div>
+//     }
+//     return null   
+// }
 export default TodoApp
-//this is to test
+//this is to test 
